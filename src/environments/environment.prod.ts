182db61ac794aeb9fa585ef82e91278517d6ccc4
wes337 <wesley.moses@happeo.com>
2021-05-20 17:36:06 +0300

    use https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiKey: 'c6257653171bbf9f7a8ee5fcf47d6ce7',
-  apiUrl: 'http://api.openweathermap.org/data/2.5',
+  apiUrl: 'https://api.openweathermap.org/data/2.5',
 };
