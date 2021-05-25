5e73941e3ba0a334b685ed845975402a494f8cb8
wes337 <wesley.moses@happeo.com>
2021-05-25 09:59:46 +0300

    write tests for search results

--- src/app/components/search-results/search-results.component.spec.ts
+++ src/app/components/search-results/search-results.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { StoreModule } from '@ngrx/store';
 import { store } from '../../store';
 import { SearchResultsComponent } from './search-results.component';
+import { City } from '../../city';
 
 describe('SearchResultsComponent', () => {
   let component: SearchResultsComponent;
@@ -26,5 +27,65 @@ describe('SearchResultsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show waiting for input', () => {
+    component.query = '';
+    component.results = [];
+    fixture.detectChanges();
+    const hostElement = fixture.nativeElement;
+    const message: HTMLInputElement = hostElement.querySelector('.card');
+    expect(message.textContent).toMatch('To begin, search for a city name.');
+  });
+
+  it('should show no results found', () => {
+    component.query = 'test';
+    component.results = [];
+    fixture.detectChanges();
+    const hostElement = fixture.nativeElement;
+    const message: HTMLInputElement = hostElement.querySelector('.card');
+    expect(message.textContent).toMatch("Couldn't find any results for test");
+  });
+
+  it('should render weather cards if results found', () => {
+    component.query = 'test';
+    component.results = [
+      {
+        id: 1,
+        name: 'halifax',
+        sys: {
+          country: 'canada',
+        },
+        coord: {
+          lat: 1,
+          lon: 1,
+        },
+        main: {
+          temp: 1,
+          feels_like: 1,
+          temp_min: 1,
+          temp_max: 1,
+          pressure: 1,
+          humidity: 1,
+        },
+        dt: 1,
+        wind: {
+          speed: 1,
+          deg: 1,
+        },
+        weather: [
+          {
+            id: 0,
+            main: 'string',
+            description: 'string',
+            icon: 'string',
+          },
+        ],
+      },
+    ];
+    fixture.detectChanges();
+    const hostElement = fixture.nativeElement;
+    const results: NodeList = hostElement.querySelectorAll('weather-card');
+    expect(results).toHaveSize(1);
   });
 });
